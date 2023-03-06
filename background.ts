2e40e4f79e78221505a3a1549bce29ed8cdf9b6c
Jaehoon Hwang (Mac) <jaehoon217@gmail.com>
2023-03-05 23:00:23 -0800

    [Fix]: Fix a bug for when a zoom setting doesn't persist across the different window sizes.
    Double calling windows seem to be the problem.
    
    Pretty weird case of something not working; probably due to window not being current window,
    but was reading the stale window data.
    
    Tags: #refactor #fix #zoom

--- background.ts
+++ background.ts
@@ -1,4 +1,5 @@
 import {
+  buildStorageRequest,
   buildStorageRequestWithNoZoom, ZoomieLocalStorage,
   ZoomieStorage, ZoomieStorageRequest
 } from './storage';
@@ -19,16 +20,10 @@ chrome.tabs.onZoomChange.addListener(
 
     const window = await chrome.windows.get(tab.windowId);
 
-    const request: ZoomieStorageRequest = {
-      zoomLevel: newZoomChange,
-      profileName: config.currentProfile.name,
-      rawUrl: tab.url!,
-      display: {
-        width: window.width,
-        height: window.height,
-        windowType: window.type,
-      }
-    };
+    const request: ZoomieStorageRequest =
+      buildStorageRequest(window, config.currentProfile.name,
+        tab, newZoomChange)
+
     storage.upsave(request);
   }
 );
@@ -40,13 +35,11 @@ chrome.windows.onBoundsChanged.addListener(
     const config = await storage.configLoad();
 
     for (const tab of tabs) {
-      if (tab.id && tab.url != undefined) {
-        chrome.windows.get(tab.windowId, async function(window) {
-          const request: ZoomieStorageRequest = buildStorageRequestWithNoZoom(
-            window, config.currentProfile.name, tab);
-          const zoomie = await storage.load(request);
-          chrome.tabs.setZoom(tab.id!, zoomie.zoomLevel);
-        });
+      if (tab.id != undefined && tab.url != undefined) {
+        const request: ZoomieStorageRequest = buildStorageRequestWithNoZoom(
+          window, config.currentProfile.name, tab);
+        const zoomie = await storage.load(request);
+        chrome.tabs.setZoom(tab.id!, zoomie.zoomLevel);
       }
     }
   }
